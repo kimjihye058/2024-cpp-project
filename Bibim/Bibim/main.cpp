1b78f5a838ba5f58145527e3664833e6dd7a6e12
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-11 13:49:24 +0900

    창 열고 닫기

--- Bibim/Bibim/main.cpp
+++ Bibim/Bibim/main.cpp
@@ -1,1 +1,19 @@
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+void main() {
+    cout << "프로그램이 시작되었습니다." << endl;
+	RenderWindow window(VideoMode(1200, 900), "SFML Window");	// sfml 창 생성
+    while (window.isOpen()) {   //SFML 메인 루프 - 윈도우가 닫힐때 까지 반복
+        Event event;    // 창 열기
+        while (window.pollEvent(event)) {       // 이벤트 처리
+            if (event.type == Event::Closed) {  // 닫기를 누르면 
+                window.close();     // 창 닫기
+                cout << "프로그램이 종료되었습니다." << endl;
+            }
+        }
+    }
+}
